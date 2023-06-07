65492df30da798acf8e448102506161a1bc3b684
Talha.k14 <Talha.k14@P-0G-0B005-0394>
2023-06-07 11:49:19 +0200

    Projektdateien hinzufügen.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class BubbleSort
+{
+    public static void Main()
+    {
+        Console.WriteLine("Geben Sie die Anzahl der Zahlen ein:");
+        int count = int.Parse(Console.ReadLine());
+
+        int[] array = new int[count];
+
+        Console.WriteLine("Geben Sie die Zahlen ein:");
+
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Unsortiertes Array:");
+        PrintArray(array);
+
+        bubbleSort(array);
+
+        Console.WriteLine("Sortiertes Array:");
+        PrintArray(array);
+    }
+
+    public static void bubbleSort(int[] array)
+    {
+        int n = array.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    // Tausche array[j] und array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
